2fa45c75e73ebb1217f50ac1458ef7155a229f35
John E. Librero <librerojohn0@gmail.com>
2025-12-12 11:37:21 +0800

    Update page.tsx

--- app/jhan/page.tsx
+++ app/jhan/page.tsx
@@ -2,7 +2,7 @@ import Info from "./info";
 export default function Page() {
   return (
     <div>
-    <h1 className="text-3x1 font-bold">Jhan Masexy Gutierez</h1>
+    <h1 className="text-3x1 font-bold">Jhan E. Librero</h1>
     <Info />
     </div>
   );
